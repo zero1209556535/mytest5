572d59fa88a8a93524f0f80d6839f19cc842ed61
zero <zero1209556535@qq.com>
2016-10-18 15:05:35 +0800

    修改SpringAnnotationMain

--- spring/src/com/spring/SpringAnnotationMain.java
+++ spring/src/com/spring/SpringAnnotationMain.java
@@ -1,5 +1,15 @@
+package com.spring;
+
+
+/**
+ * Created by lenovo on 2016/10/18.
+ */
 public class SpringAnnotationMain{
-  void springMain{
- system.out.println("this is springMain test");
-}
+    void springMain(){
+        System.out.println("this is springMain test");
+    }
+
+    void springMain2(){
+        System.out.println("this is springMain test2");
+    }
 }
