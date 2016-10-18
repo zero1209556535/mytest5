8da0ace788e0f1960eeb1c059c22db2d4a84c074
zero <zero1209556535@qq.com>
2016-10-18 14:54:37 +0800

    spring提交到git上

---
+++ spring/src/com/spring/SpringAnnotation.java
@@ -0,0 +1,10 @@
+package com.spring;
+
+/**
+ * Created by lenovo on 2016/10/18.
+ */
+public class SpringAnnotation {
+    void spring(){
+        System.out.println("this is spring test");
+    }
+}
